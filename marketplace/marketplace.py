87688ed9d85fd9671a931e828fa6d1b2d99ec5a4
smolkin895 <smol140791@ya.ru>
2022-04-17 01:24:14 +0300

    1.0

---
+++ marketplace/marketplace.py
@@ -0,0 +1,37 @@
+# marketplace/marketplace.py
+import os
+
+import grpc
+
+from flask import Flask, render_template
+
+
+from recommendations.recommendations_pb2 import BookCategory, RecommendationRequest
+from recommendations.recommendations_pb2_grpc import RecommendationsStub
+
+app = Flask(__name__)
+
+recommendations_host = os.getenv("RECOMMENDATIONS_HOST", "localhost")
+recommendations_channel = grpc.insecure_channel(
+    f"{recommendations_host}:50051"
+)
+recommendations_client = RecommendationsStub(recommendations_channel)
+
+
+@app.route("/")
+def render_homepage():
+    recommendations_request = RecommendationRequest(
+        user_id=1, category=BookCategory.MYSTERY, max_results=3
+    )
+    recommendations_response = recommendations_client.Recommend(
+        recommendations_request
+    )
+    return render_template(
+        "homepage.html",
+        recommendations=recommendations_response.recommendations,
+    )
+
+
+
+if __name__ == '__main__':
+    app.run()
